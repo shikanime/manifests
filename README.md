21a2db77b0e8700987c2ee2b0aebe5f170d679a9
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-01-29 17:50:32 +0100

    Pull internal code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Manifests
+<!-- markdownlint-disable first-line-heading -->
 
-Collection of application deployment manifests for Kubernetes.
+![header.png](https://raw.githubusercontent.com/shikanime/shikanime/main/assets/github-header.png)
+
+<!-- markdownlint-enable first-line-heading -->
+
+## Manifests
+
+Hey 🌸 I'm Shikanime Deva, this is the Kubernetes manifests of my clusters.
---
+++ apps/postgres/base/postgres/docker-entrypoint-initdb.d/vaultwarden.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+postgres_vaultwarden_db=${POSTGRES_VAULTWARDEN_DB:-vaultwarden}
+postgres_vaultwarden_user=${POSTGRES_VAULTWARDEN_USER:-vaultwarden}
+postgres_vaultwarden_password=$POSTGRES_VAULTWARDEN_PASSWORD
+
+psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER:-postgres}" --dbname "${POSTGRES_DB:-postgres}" <<-EOSQL
+    CREATE USER "$postgres_vaultwarden_user" WITH PASSWORD '$postgres_vaultwarden_password';
+    CREATE DATABASE "$postgres_vaultwarden_db" OWNER "$postgres_vaultwarden_user";
+EOSQL
