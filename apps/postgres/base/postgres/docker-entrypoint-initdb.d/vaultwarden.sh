2dedfd6e4d4377edd2771ddb42e2f587eecaf495
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-05-09 20:16:22 +0200

    Add Gitea

--- apps/postgres/base/postgres/docker-entrypoint-initdb.d/vaultwarden.sh
+++ apps/postgres/base/postgres/docker-entrypoint-initdb.d/vaultwarden.sh
@@ -7,6 +7,6 @@ postgres_vaultwarden_user=${POSTGRES_VAULTWARDEN_USER:-vaultwarden}
 postgres_vaultwarden_password=$POSTGRES_VAULTWARDEN_PASSWORD
 
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER:-postgres}" --dbname "${POSTGRES_DB:-postgres}" <<-EOSQL
-    CREATE USER "$postgres_vaultwarden_user" WITH PASSWORD '$postgres_vaultwarden_password';
+    CREATE USER "$postgres_vaultwarden_user" WITH PASSWORD "$postgres_vaultwarden_password";
     CREATE DATABASE "$postgres_vaultwarden_db" OWNER "$postgres_vaultwarden_user";
 EOSQL
