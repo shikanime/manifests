c15441ada0df8e46bc2a7ada254316766af599b7
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-09-04 23:17:01 +0200

    Migrate infrastructure bootstrap scripts

---
+++ infra/variable.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  type        = string
+  description = "The name of the cluster"
+}
+
+variable "app" {
+  type        = string
+  description = "The name of the application"
+}
+
+variable "buckets" {
+  type        = object({ longhorn = string })
+  description = "The name of the buckets"
+}
+
+variable "secrets" {
+  type = object({
+    prometheus_credentails = string
+    loki_credentails       = string
+    tempo_credentails      = string
+    tailscale_oauth_client = string
+  })
+  description = "The name of the secrets"
+}
