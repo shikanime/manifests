c8e71dfdf390d46665c31d9ad1fd99e090e3e1c1
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-09-04 23:17:01 +0200

    Migrate infrastructure bootstrap scripts

---
+++ infra/version.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket                      = "shikanime-studio-fr-par-opentofu-state"
+    key                         = "nishir/terraform.tfstate"
+    region                      = "fr-par"
+    endpoint                    = "s3.fr-par.scw.cloud"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "~> 2.43.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32.0"
+    }
+  }
+}
