4f4bbdab61169791112e46f2a1378fba3ce33bfd
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-11-19 11:32:49 +0000

    Remove old clusters
    [ghstack-poisoned]

--- infra/ishtar/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  type        = string
-  description = "The name of the cluster"
-}
-
-variable "secrets" {
-  type        = object({ tailscale_oauth_client = string })
-  description = "The name of the secrets"
-}
