5e5d48ed4d155702e1ba41eee67cb38b73645584
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-08 17:05:33 +0100

    None on "Migrate to Cloudflare"
    [ghstack-poisoned]

--- infra/nishir/secret.tf
+++ infra/nishir/secret.tf
@@ -17,8 +17,8 @@ resource "scaleway_secret" "nishir_api_token" {
 resource "scaleway_secret_version" "nishir_api_token" {
   secret_id = scaleway_secret.nishir_api_token.id
   data = jsonencode({
-    client_id     = cloudflare_api_token.k8s.id
-    client_secret = cloudflare_api_token.k8s.value
+    clientId     = cloudflare_api_token.k8s.id
+    clientSecret = cloudflare_api_token.k8s.value
   })
 }
 
