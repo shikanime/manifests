72ff88623d6ec2765482159c2a8b8c261b949c41
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-12 15:52:29 +0000

    Extract Nix Devenv config

---
+++ nix/devenv.nix
@@ -0,0 +1,64 @@
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages = {
+        default = pkgs.stdenv.mkDerivation {
+          name = "manifests";
+          src = ./.;
+          installPhase = ''
+            mkdir -p $out/share
+            cp -r apps clusters infra $out/share
+          '';
+        };
+      };
+      treefmt = {
+        projectRootFile = "flake.nix";
+        enableDefaultExcludes = true;
+        programs = {
+          actionlint.enable = true;
+          deadnix.enable = true;
+          dos2unix.enable = true;
+          nixfmt.enable = true;
+          prettier.enable = true;
+          shfmt.enable = true;
+          statix.enable = true;
+          terraform.enable = true;
+        };
+        settings.global.excludes = [
+          "*.excalidraw"
+          "*.terraform.lock.hcl"
+          ".gitattributes"
+          "LICENSE"
+        ];
+      };
+      devenv.shells.default = {
+        containers = pkgs.lib.mkForce { };
+        languages = {
+          terraform = {
+            enable = true;
+            package = pkgs.opentofu;
+          };
+          nix.enable = true;
+        };
+        cachix = {
+          enable = true;
+          push = "shikanime";
+        };
+        pre-commit.hooks = {
+          shellcheck.enable = true;
+          tflint.enable = true;
+        };
+        packages = [
+          pkgs.gh
+          pkgs.kubectl
+          pkgs.kubernetes-helm
+          pkgs.kustomize
+          pkgs.scaleway-cli
+          pkgs.skaffold
+          pkgs.skopeo
+          pkgs.yq-go
+        ];
+      };
+    };
+}
