e8fddf5559353576c1f83cbe1ceaa13209c1bfce
William Phetsinorath <william.phetsinorath@shikanime.studio>
2024-12-16 02:35:32 +0100

    None on "Move secret config to Tofu manifests"
    [ghstack-poisoned]

--- infra/nishir/manifest.tf
+++ infra/nishir/manifest.tf
@@ -63,8 +63,8 @@ resource "kubernetes_manifest" "longhorn" {
       bootstrap       = false
       valuesContent = jsonencode({
         defaultSettings = {
-          backupTarget                 = local.longhorn_b2_backup_target
-          backupTargetCredentialSecret = one(kubernetes_secret.longhorn_b2_backups.metadata).name
+          backupTarget                 = local.longhorn_backup_target
+          backupTargetCredentialSecret = one(kubernetes_secret.longhorn_cf_backups.metadata).name
         }
       })
     }
--- infra/nishir/storage.tf
+++ infra/nishir/storage.tf
@@ -1,7 +1,5 @@
 locals {
-  longhorn_b2_backup_target = "s3://${b2_bucket.longhorn_backups.bucket_name}@${local.longhorn_b2_backup_region}/"
-  longhorn_b2_backup_region = "eu-central"
-  longhorn_cf_backup_target = "s3://${cloudflare_r2_bucket.longhorn_backups.name}@${cloudflare_r2_bucket.longhorn_backups.location}/"
+  longhorn_backup_target = "s3://${cloudflare_r2_bucket.longhorn_backups.name}@${cloudflare_r2_bucket.longhorn_backups.location}/"
 }
 
 resource "cloudflare_r2_bucket" "longhorn_backups" {
@@ -14,22 +12,4 @@ resource "cloudflare_r2_bucket" "etcd_backups" {
   account_id = var.account
   name       = "nishir-etcd-backups"
   location   = "WEUR"
-}
-
-resource "b2_bucket" "longhorn_backups" {
-  bucket_name = "shikanime-studio-nishir-longhorn-backups"
-  bucket_type = "allPrivate"
-  default_server_side_encryption {
-    algorithm = "AES256"
-    mode      = "SSE-B2"
-  }
-}
-
-resource "b2_bucket" "etcd_backups" {
-  bucket_name = "shikanime-studio-nishir-etcd-backups"
-  bucket_type = "allPrivate"
-  default_server_side_encryption {
-    algorithm = "AES256"
-    mode      = "SSE-B2"
-  }
 }
