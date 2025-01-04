4d98151e25347f56185bb012ee301b4d876f444f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-01-04 02:34:10 +0100

    Destroy Cloudflare infrastructure
    [ghstack-poisoned]

--- infra/nishir/iam.tf
+++ infra/nishir/iam.tf
@@ -1,33 +1,3 @@
-data "cloudflare_api_token_permission_groups" "default" {}
-
-resource "cloudflare_api_token" "longhorn_backupstore" {
-  name = "${var.display_name} Longhorn Backupstore"
-
-  policy {
-    permission_groups = [
-      data.cloudflare_api_token_permission_groups.default.account["Workers R2 Storage Read"],
-      data.cloudflare_api_token_permission_groups.default.account["Workers R2 Storage Write"],
-    ]
-    resources = {
-      "com.cloudflare.edge.r2.bucket.${var.account}_default_${cloudflare_r2_bucket.longhorn_backups.id}" = "*"
-    }
-  }
-}
-
-resource "cloudflare_api_token" "etcd_snapshot" {
-  name = "${var.display_name} ETCD Snapshot"
-
-  policy {
-    permission_groups = [
-      data.cloudflare_api_token_permission_groups.default.account["Workers R2 Storage Read"],
-      data.cloudflare_api_token_permission_groups.default.account["Workers R2 Storage Write"],
-    ]
-    resources = {
-      "com.cloudflare.edge.r2.bucket.${var.account}_default_${cloudflare_r2_bucket.etcd_backups.id}" = "*"
-    }
-  }
-}
-
 resource "grafana_cloud_access_policy" "kubernetes" {
   region       = "eu"
   name         = "stack-${var.sack}-integration-${var.name}"
