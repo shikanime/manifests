21c008a568f71e2ab854e8cc90c6b2ff7d2f6560
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-01-10 15:08:33 +0000

    Update base for None on "Fix Grafana endpoints"
    [ghstack-poisoned]

--- infra/nishir/storage.tf
+++ infra/nishir/storage.tf
@@ -1,6 +1,6 @@
 locals {
-  longhorn_backup_target = "s3://${aws_s3_bucket.longhorn_backups.bucket}@${var.regions.longhorn_backups}/"
-  longhorn_endpoints     = "https://${var.regions.longhorn_backups}.your-objectstorage.com"
+  longhorn_backup_target = "s3://${aws_s3_bucket.longhorn_backups.bucket}@${var.regions.aws_s3_bucket}/"
+  longhorn_endpoints     = "https://${var.regions.aws_s3_bucket}.your-objectstorage.com"
 }
 
 resource "random_id" "longhorn_backups" {
