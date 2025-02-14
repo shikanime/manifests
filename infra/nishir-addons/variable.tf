77873a4a76925eff27e6a7f70a7e52b45d80b447
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-02-14 17:48:41 +0100

    Extract services from air gapped cluster
    [ghstack-poisoned]

--- infra/nishir-addons/variable.tf
+++ infra/nishir-addons/variable.tf
@@ -1,65 +1,44 @@
-variable "project" {
-  type        = string
-  description = "Project name"
-  default     = "shikanime-studio"
-}
-
-variable "name" {
-  type        = string
-  description = "Name of the cluster"
-  default     = "nishir"
-}
-
-variable "display_name" {
-  type        = string
-  description = "Display name of the cluster"
-  default     = "Nishir"
-}
-
-variable "stack" {
-  type        = string
-  description = "Grafana stack ID"
-  default     = "370431"
-}
-
-variable "regions" {
+variable "tailscale" {
   type = object({
-    grafana_cloud_access_policy = string
-    aws_s3_bucket               = string
+    client_id     = string
+    client_secret = string
   })
-  description = "Resource regions"
-  default = {
-    grafana_cloud_access_policy = "eu"
-    aws_s3_bucket               = "fsn1"
-  }
+  sensitive = true
 }
 
-variable "endpoints" {
+variable "longhorn" {
   type = object({
-    prometheus = string
-    loki       = string
-    tempo      = string
-    s3         = string
+    access_key_id     = string
+    backup_target     = string
+    endpoints          = string
+    secret_access_key = string
   })
-  description = "Resource API endpoints"
-  default = {
-    prometheus = "https://prometheus-prod-01-eu-west-0.grafana.net"
-    loki       = "https://logs-prod-eu-west-0.grafana.net"
-    tempo      = "https://tempo-eu-west-0.grafana.net"
-    s3         = "https://fsn1.your-objectstorage.com"
-  }
+  sensitive = true
 }
 
-variable "secrets" {
+variable "prometheus" {
   type = object({
-    tailscale_operator_oauth_client = string
-    longhorn_backupstore_s3_creds   = string
-    vaultwarden_admin_token         = string
+    endpoint = string
+    password = string
+    username = string
   })
-  description = "Scaleway secrets ID"
-  default = {
-    tailscale_operator_oauth_client = "371097be-4fd8-4e8f-a3f3-232485c96082"
-    longhorn_backupstore_s3_creds   = "b5d61a39-cf9f-44dd-98f9-7d8f73852bde"
-    vaultwarden_admin_token         = "4e4b6a04-8885-4579-ad8a-a25d72f63758"
-  }
+  sensitive = true
+}
+
+variable "loki" {
+  type = object({
+    endpoint = string
+    password = string
+    username = string
+  })
+  sensitive = true
+}
+
+variable "tempo" {
+  type = object({
+    endpoint = string
+    password = string
+    username = string
+  })
+  sensitive = true
 }
---
+++ infra/nishir-services/outputs.tf
@@ -0,0 +1,44 @@
+output "tailscale" {
+  value = {
+    client_id     = local.tailscale_operator_oauth_client_data.clientId
+    client_secret = local.tailscale_operator_oauth_client_data.clientSecret
+  }
+  sensitive = true
+}
+
+output "longhorn" {
+  value = {
+    backup_target     = local.longhorn_backup_target
+    access_key_id     = local.longhorn_backupstore_s3_creds_data.access_key_id
+    secret_access_key = local.longhorn_backupstore_s3_creds_data.secret_access_key
+    endpoints         = var.endpoints.s3
+  }
+  sensitive = true
+}
+
+output "prometheus" {
+  value = {
+    endpoint = var.endpoints.prometheus
+    password = resource.grafana_cloud_access_policy_token.kubernetes.token
+    username = data.grafana_data_source.prometheus.basic_auth_username
+  }
+  sensitive = true
+}
+
+output "loki" {
+  value = {
+    endpoint = var.endpoints.loki
+    password = resource.grafana_cloud_access_policy_token.kubernetes.token
+    username = data.grafana_data_source.loki.basic_auth_username
+  }
+  sensitive = true
+}
+
+output "tempo" {
+  value = {
+    endpoint = var.endpoints.tempo
+    password = resource.grafana_cloud_access_policy_token.kubernetes.token
+    username = data.grafana_data_source.tempo.basic_auth_username
+  }
+  sensitive = true
+}
