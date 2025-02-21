51f2212b75a9b25dfb3a572e4a9d80ad33ca9dbe
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-02-21 02:04:18 +0100

    Add cluster nodes
    [ghstack-poisoned]

--- infra/nishir-addons/node.tf
+++ infra/nishir-addons/node.tf
@@ -1,13 +1,14 @@
 resource "local_file" "config" {
-  filename = "${path.module}/.terraform/tmp/nodes/config.yaml"
-  content = templatefile("${path.module}/templates/nodes/config.yaml.tftpl", {
-    tls_san         = var.endpoints.nishir
-    node_ip         = var.ip_addresses.nishir
+  filename = "${path.module}/.terraform/tmp/nodes/primary/config.yaml"
+  content = templatefile("${path.module}/templates/nodes/primary/config.yaml.tftpl", {
     etcd_access_key = var.etcd_snapshot.access_key_id
     etcd_bucket     = var.etcd_snapshot.bucket
     etcd_endpoint   = var.etcd_snapshot.endpoint
     etcd_region     = var.etcd_snapshot.region
     etcd_secret_key = var.etcd_snapshot.secret_access_key
+    node_ip         = var.ip_addresses.nishir
+    tls_san         = var.endpoints.nishir
+    token           = var.k3s.token
   })
   file_permission = "0600"
 }
@@ -68,5 +69,61 @@ resource "terraform_data" "nishir" {
   provisioner "file" {
     content     = local_file.shikanime.content
     destination = "/mnt/nishir/rancher/k3s/server/manifests/shikanime.yaml"
+  }
+}
+
+resource "local_file" "fushi" {
+  filename = "${path.module}/.terraform/tmp/nodes/secondary/config.yaml"
+  content = templatefile("${path.module}/templates/nodes/secondary/config.yaml.tftpl", {
+    node_ip = var.ip_addresses.fushi
+    server  = "https://${var.endpoints.nishir}:6443"
+    tls_san = var.endpoints.fushi
+    token   = var.k3s.token
+  })
+  file_permission = "0600"
+}
+
+resource "terraform_data" "fushi" {
+  triggers_replace = {
+    config_content = local_file.fushi.content
+  }
+
+  connection {
+    type = "ssh"
+    user = "root"
+    host = "fushi"
+  }
+
+  provisioner "file" {
+    content     = local_file.fushi.content
+    destination = "/etc/rancher/k3s/config.yaml"
+  }
+}
+
+resource "local_file" "minish" {
+  filename = "${path.module}/.terraform/tmp/nodes/secondary/config.yaml"
+  content = templatefile("${path.module}/templates/nodes/secondary/config.yaml.tftpl", {
+    node_ip = var.ip_addresses.minish
+    server  = "https://${var.endpoints.nishir}:6443"
+    tls_san = var.endpoints.minish
+    token   = var.k3s.token
+  })
+  file_permission = "0600"
+}
+
+resource "terraform_data" "minish" {
+  triggers_replace = {
+    config_content = local_file.minish.content
+  }
+
+  connection {
+    type = "ssh"
+    user = "root"
+    host = "minish"
+  }
+
+  provisioner "file" {
+    content     = local_file.minish.content
+    destination = "/etc/rancher/k3s/config.yaml"
   }
 }
