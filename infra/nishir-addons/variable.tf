ca259298f1f07d100d972ff4da91aa6207bf82c4
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-02-21 14:09:38 +0100

    Update Minish IP
    ghstack-source-id: d43a96a2b49e6717c030ff53f4c6c93efa7717fa
    Pull Request resolved: https://github.com/shikanime/manifests/pull/250

--- infra/nishir-addons/variable.tf
+++ infra/nishir-addons/variable.tf
@@ -22,7 +22,7 @@ variable "ip_addresses" {
   default = {
     nishir = ["100.93.169.85", "fd7a:115c:a1e0::c301:a955"]
     fushi  = ["100.78.148.86", "fd7a:115c:a1e0::8001:9456"]
-    minish = ["100.82.28.89", "fd7a:115c:a1e0::8501:1c59"]
+    minish = ["100.115.159.112", "fd7a:115c:a1e0::d101:9f71"]
   }
 }
 
