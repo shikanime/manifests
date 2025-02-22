e61c54d211373708d5df369a22c471f8a7654d8e
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-02-22 16:26:39 +0100

    Rename Nishir addons to cluster
    ghstack-source-id: ad68965098ae04e7c17d77a8eb309ed613acef7e
    Pull Request resolved: https://github.com/shikanime/manifests/pull/259

--- infra/nishir-cluster/version.tf
+++ infra/nishir-cluster/version.tf
@@ -4,7 +4,7 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "shikanime-studio"
     workspaces {
-      name = "nishir-addons"
+      name = "nishir-cluster"
     }
   }
   required_providers {
