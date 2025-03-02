565af303b0d53bfcf8cf4e4867d506ca1a6e4ccd
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-02 17:21:54 +0100

    Update base for None on "Migrate Tailscale config to Terraform"
    [ghstack-poisoned]

--- infra/shana-cluster/install.sh
+++ infra/shana-cluster/install.sh
@@ -6,8 +6,8 @@ set -o pipefail
 
 # Get SSH keys from OpenTofu JSON output
 output=$(tofu output -json)
-echo "${output}" | jq -r '.ssh_private_key.value' > "$HOME/.ssh/shana_ed25519"
-echo "${output}" | jq -r '.ssh_public_key.value' > "$HOME/.ssh/shana_ed25519.pub"
+echo "${output}" | jq -r '.ssh_private_key.value' >"$HOME/.ssh/shana_ed25519"
+echo "${output}" | jq -r '.ssh_public_key.value' >"$HOME/.ssh/shana_ed25519.pub"
 
 # Set correct permissions
 chmod 600 ~/.ssh/shana_ed25519
