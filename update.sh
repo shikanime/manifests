fa5f7da144ca70e55eb808e8100ff46abd0c6790
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-07 15:10:42 +0100

    Refresh gitignore to update script
    ghstack-source-id: 11278fcc96c8587339f420e014a13c9e63c410ca
    Pull Request resolved: https://github.com/shikanime/manifests/pull/282

--- update.sh
+++ update.sh
@@ -4,6 +4,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Update gitignore
+gitnr create \
+  ghc:Nix \
+  repo:shikanime/gitignore/refs/heads/main/Devenv.gitignore \
+  tt:jetbrains+all \
+  tt:Linux \
+  tt:Macos \
+  tt:Terraform \
+  tt:vim \
+  tt:visualstudiocode \
+  tt:Windows >.gitignore
+
 for dir in "$(dirname "$0")"/apps/*/base; do
   if [ -f "$dir/update.sh" ]; then
     bash "$dir/update.sh" 2>&1 |
