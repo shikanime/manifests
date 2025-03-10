936a08befe6c96beca7b645dad8a0bf6ea65eef7
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-10 20:23:29 +0100

    Avoid longhornctl rebuild
    [ghstack-poisoned]

--- nix/pkgs/longhornctl/update.sh
+++ nix/pkgs/longhornctl/update.sh
@@ -18,10 +18,17 @@ COMMIT=$(
 
 GIT_PREFETCH=$(nix-prefetch-url --unpack https://github.com/longhorn/cli/archive/${COMMIT}.tar.gz)
 GIT_HASH=$(nix hash convert --hash-algo sha256 --to sri ${GIT_PREFETCH})
-BUILD_DATE=$(date --iso-8601=seconds)
 
 sed -i \
   -e "s|version = \".*\"|version = \"${LATEST_VERSION:-}\"|" \
   -e "s|hash = \".*\"|hash = \"${GIT_HASH}\"|" \
-  -e "s|\"-X github.com/longhorn/cli/meta.BuildDate=.*\"|\"-X github.com/longhorn/cli/meta.BuildDate=${BUILD_DATE}\"|" \
   "$(dirname "$0")"/default.nix
+
+# Only update build date if default.nix was modified
+if [ -n "$(git diff "$(dirname "$0")/default.nix")" ]; then
+  BUILD_DATE=$(date --iso-8601=seconds)
+
+  sed -i \
+    -e "s|\"-X github.com/longhorn/cli/meta.BuildDate=.*\"|\"-X github.com/longhorn/cli/meta.BuildDate=${BUILD_DATE}\"|" \
+    "$(dirname "$0")"/default.nix
+fi
