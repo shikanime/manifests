8bb7d4e696c6feb47502fed03fa24e37e8f3c6f0
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-11 12:10:30 +0100

    Fix update script varible case
    ghstack-source-id: e5721796160cce00ef9bfaa4c60c754d2f3b9ba4
    Pull Request resolved: https://github.com/shikanime/manifests/pull/318

--- infra/nishir-followers/install.sh
+++ infra/nishir-followers/install.sh
@@ -5,15 +5,15 @@ set -o nounset
 set -o pipefail
 
 # Find the nishir server name from tailscale
-server=$(tailscale status -json | jq -r '.Peer[] | select(.HostName == "nishir") | .DNSName' | sed 's/\.$//')
+SERVER=$(tailscale status -json | jq -r '.Peer[] | select(.HostName == "nishir") | .DNSName' | sed 's/\.$//')
 
 # SSH into nishir and retrieve the server token
-token=$(ssh "root@${server}" "cat /var/lib/rancher/k3s/server/token")
+TOKEN=$(ssh "root@${SERVER}" "cat /var/lib/rancher/k3s/server/token")
 
 # Create JSON using jq
 jq -n \
-  --arg token "${token}" \
-  --arg server "${server}" \
+  --arg token "${TOKEN}" \
+  --arg server "${SERVER}" \
   '{k3s: {token: $token, server: $server}}' >"$(dirname "$0")/terraform.tfvars.json"
 
 # Set secure permissions for the tfvars file
