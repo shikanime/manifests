ae59dbc83aff2548bf80b65d7ac04e1c904e0e93
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-14 18:55:52 +0100

    Update deps
    [ghstack-poisoned]

--- nix/pkgs/longhornctl/default.nix
+++ nix/pkgs/longhornctl/default.nix
@@ -24,7 +24,7 @@ buildGoModule rec {
     "-w"
     "-X github.com/longhorn/cli/meta.Version=v${version}"
     "-X github.com/longhorn/cli/meta.GitCommit=${src.rev}"
-    "-X github.com/longhorn/cli/meta.BuildDate=2025-03-08T15:00:05+00:00"
+    "-X github.com/longhorn/cli/meta.BuildDate=2025-03-14T18:38:33+01:00"
   ];
 
   postInstall = ''
