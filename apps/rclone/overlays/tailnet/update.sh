191901953ef4e2d08e398fe7cbbb35cc157ebec9
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-14 18:58:51 +0100

    Fix Rclone IP sync update script
    ghstack-source-id: 8cb04591482486d50e0e580b5c9ca36ccd3b4910
    Pull Request resolved: https://github.com/shikanime/manifests/pull/329

--- apps/rclone/overlays/tailnet/update.sh
+++ apps/rclone/overlays/tailnet/update.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o pipefail
 
 # Get Tailscale status for FTP device in JSON format
-FTP_IP=$(tailscale status -json | jq -r '.Peer[] | select(.HostName=="ftp") | .TailscaleIPs[0]')
+FTP_IP=$(tailscale status -json | jq -r '({("\(.Self.PublicKey)"): .Self} + .Peer)[] | select(.HostName=="ftp") | .TailscaleIPs[0]')
 
 # Update the StatefulSet YAML with the new IP
 sed -i \
-  -e "s/value: \"[0-9.]*\"/value: \"${FTP_IP}\"/" "$(dirname "$0")/patch-sts.yaml"
+  -e "s/value: \"[0-9.]*\"/value: \"${FTP_IP}\"/" "$(dirname "$0")/patch-deploy.yaml"
