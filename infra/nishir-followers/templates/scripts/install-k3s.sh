b25abd0a73e6d88b34e93f772491db8864e4ea0b
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-03-16 16:19:58 +0100

    Migrate control plane and master components out of followers
    ghstack-source-id: 0859fac4dea9b75190e8bc87ea0013296568e388
    Pull Request resolved: https://github.com/shikanime/manifests/pull/337

--- infra/nishir-followers/templates/scripts/install-k3s.sh
+++ infra/nishir-followers/templates/scripts/install-k3s.sh
@@ -17,14 +17,6 @@ apt-get install -y \
   jq
 
 # Install k3s
-curl -sfL https://get.k3s.io | K3S_TOKEN="${token}" \
-  sh -s - \
-  --cluster-cidr "10.42.0.0/16,2001:cafe:42::/56" \
-  --disable-etcd \
-  --flannel-backend "host-gw" \
-  --flannel-ipv6-masq \
-  --node-ip "$(tailscale status -json | jq -r '.TailscaleIPs | join(",")')" \
-  --protect-kernel-defaults \
-  --server "${server}" \
-  --service-cidr "10.43.0.0/16,2001:cafe:43::/112" \
-  --tls-san "$(tailscale status -json | jq -r '.CertDomains | join(",")')"
+curl -sfL https://get.k3s.io |
+  K3S_TOKEN="${token}" K3S_URL="${server}" \
+    sh -s -
