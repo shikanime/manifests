c302b28648f6b91b64d7a22814fd6ee329f35ce4
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-15 02:18:02 +0200

    Refactor update scripts in nu
    [ghstack-poisoned]

--- apps/qbittorrent/base/update.sh
+++ apps/qbittorrent/base/update.sh
@@ -1,24 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env nix
+#! nix shell nixpkgs#nushell nixpkgs#skopeo nixpkgs#kustomize --command nu
 
-# Define container images to check
-declare -A IMAGES=(
-  ["qbittorrent"]="lscr.io/linuxserver/qbittorrent"
-)
+def --wrapped kustomize [...rest] {
+    cd $env.FILE_PWD
+    exec kustomize ...$rest
+}
 
-for IMAGE_NAME in "${!IMAGES[@]}"; do
-  FULL_IMAGE="${IMAGES[$IMAGE_NAME]}"
-  # Skip version 20.04.1, 14.3.9 as it is a known wrong tag
-  LATEST_VERSION=$(
-    skopeo list-tags "docker://${FULL_IMAGE}" |
-      jq -r '.Tags | map(select(test("^[0-9]+\\.[0-9]+\\.[0-9]+$") and . != "20.04.1" and . != "14.3.9")) | sort_by(split(".") | map(tonumber)) | last'
-  )
-  if [[ -z $LATEST_VERSION ]]; then
-    echo "Image '$FULL_IMAGE' not found in registry."
-  else
-    (cd "$(dirname "$0")" &&
-      kustomize edit set image "${IMAGE_NAME}=${FULL_IMAGE}:${LATEST_VERSION}")
-    yq -i \
-      ".labels.[].pairs.[\"app.kubernetes.io/version\"] = \"${LATEST_VERSION}\"" \
-      "$(dirname "$0")/kustomization.yaml"
-  fi
-done
+{ qbittorrent: lscr.io/linuxserver/qbittorrent } | items { |name, image|
+    let latest_version = (
+        ^skopeo list-tags $"docker://($image)"
+        | from json
+        | get Tags
+        | where ($it  =~ ^[0-9]+\.[0-9]+\.[0-9]+$)
+        | where ($it != "20.04.1" and $it != "14.3.9")
+        | sort-by { |tag| $tag | split row "." | each { |n| ($n | into int) } }
+        | last
+    )
+
+    if ($latest_version | is-empty) {
+        print $"Image '($image)' not found in registry."
+        return
+    }
+
+    kustomize edit set image $"($name)=($image):($latest_version)"
+
+    open $"($env.FILE_PWD)/kustomization.yaml"
+    | update labels { |label|
+        $label | update pairs { |it|
+            $it | upsert "app.kubernetes.io/version" $latest_version
+        }
+    }
+    | save -f kustomization.yaml
+}
