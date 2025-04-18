71007626cb8db4f19df297603fd79ca954094e04
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-18 18:55:47 +0200

    None on "Migrate to RKE2"
    [ghstack-poisoned]

--- infra/nishir-followers/install.sh
+++ infra/nishir-followers/install.sh
@@ -4,17 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# SSH into nishir and retrieve the server token
+NODE_TOKEN=$(ssh "root@${SERVER}" "cat /var/lib/rancher/rke2/server/node-token")
+
 # Find the nishir server name from tailscale
 SERVER=$(tailscale status -json | jq -r '.Peer[] | select(.HostName == "nishir") | .DNSName' | sed 's/\.$//')
 
-# SSH into nishir and retrieve the server token
-TOKEN=$(ssh "root@${SERVER}" "cat /var/lib/rancher/rke2/server/node-token")
-
 # Create JSON using jq
 jq -n \
-  --arg token "${TOKEN}" \
+  --arg node_token "${NODE_TOKEN}" \
   --arg server "${SERVER}" \
-  '{rke2: {token: $token, server: $server}}' >"$(dirname "$0")/terraform.tfvars.json"
+  '{rke2: {node_token: $node_token, server: $server}}' >"$(dirname "$0")/terraform.tfvars.json"
 
 # Set secure permissions for the tfvars file
 chmod 600 "$(dirname "$0")/terraform.tfvars.json"
