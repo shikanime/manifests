0d4fb962e9d66b0d722354745cf7d8e4838e7c11
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-18 18:55:56 +0200

    Remove Shana cluster
    ghstack-source-id: 77fb9fce2d60be3d385c4d9882dd2cba73495b80
    Pull Request resolved: https://github.com/shikanime/manifests/pull/383

--- infra/shana-leader/ssh.tf
+++
@@ -1,8 +0,0 @@
-resource "hcloud_ssh_key" "default" {
-  name       = var.display_name
-  public_key = tls_private_key.default.public_key_openssh
-}
-
-resource "tls_private_key" "default" {
-  algorithm = "ED25519"
-}
--- infra/shana-services/storage.tf
+++
@@ -1,8 +0,0 @@
-resource "random_id" "etcd_backups" {
-  byte_length = 4
-  prefix      = "${var.project}-${var.name}-etcd-backups-"
-}
-
-resource "aws_s3_bucket" "etcd_backups" {
-  bucket = random_id.etcd_backups.hex
-}
