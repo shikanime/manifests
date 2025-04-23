30da47f217da5ea3d96194b668000014041af9f5
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-23 19:20:41 +0200

    Update base for None on "Set Flannel interface"
    [ghstack-poisoned]

--- infra/nishir-addons/install.sh
+++ infra/nishir-addons/install.sh
@@ -15,9 +15,11 @@ tofu -chdir="$(dirname "$0")/../nishir-services" output -json |
     select(.key | IN(
       "name",
       "endpoints",
+      "hetzner",
       "longhorn_backupstore",
       "prometheus",
       "loki",
+      "tailscale_operator",
       "tempo",
       "vaultwarden"
     )) |
--- infra/nishir-addons/variable.tf
+++ infra/nishir-addons/variable.tf
@@ -14,6 +14,14 @@ variable "endpoints" {
   }
 }
 
+variable "hetzner" {
+  type = object({
+    hcloud_token = string
+  })
+  description = "Hetzner Cloud API token"
+  sensitive   = true
+}
+
 variable "longhorn_backupstore" {
   type = object({
     access_key_id     = string
@@ -43,6 +51,15 @@ variable "loki" {
     username = string
   })
   description = "Loki log aggregation system authentication"
+  sensitive   = true
+}
+
+variable "tailscale_operator" {
+  type = object({
+    client_id     = string
+    client_secret = string
+  })
+  description = "Tailscale operator authentication credentials"
   sensitive   = true
 }
 
