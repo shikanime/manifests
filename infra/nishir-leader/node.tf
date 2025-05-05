cf54207a47d47034a1d32a6488472ae73a4c5ffc
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-05-06 01:51:45 +0200

    Update base for Update on "Add Syncthing container build"
    [ghstack-poisoned]

--- infra/nishir-leader/node.tf
+++ infra/nishir-leader/node.tf
@@ -1,85 +1,20 @@
-resource "local_file" "config_rke2" {
-  filename = "${path.module}/.terraform/tmp/configs/rke2/config.yaml.tftpl"
-  content = templatefile("${path.module}/templates/configs/rke2/config.yaml.tftpl", {
-    etcd_s3_access_key_id     = var.etcd_snapshot.access_key_id
-    etcd_s3_bucket            = var.etcd_snapshot.bucket
-    etcd_s3_endpoint          = var.etcd_snapshot.endpoint
-    etcd_s3_region            = var.etcd_snapshot.region
-    etcd_s3_secret_access_key = var.etcd_snapshot.secret_access_key
-    node_ip                   = var.rke2.node_ip
-    node_labels = {
-      "beta.kubernetes.io/instance-type" = "rpi5-large"
-      "node.kubernetes.io/instance-type" = "rpi5-large"
-    }
-    tls_san = var.rke2.tls_san
-  })
-  file_permission = "0600"
-}
+locals {
+  install_commands = [
+    "systemd-tmpfiles --create",
+    "sysctl --system",
+    "apt-get update -y",
+    "apt-get install -y open-iscsi nfs-common cryptsetup dmsetup jq",
+    "curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE=server sh -s -",
+    "systemctl enable rke2-server.service",
+    "mkdir -p /etc/rancher/rke2"
+  ]
 
-resource "local_file" "config_sysctl_k8s" {
-  filename        = "${path.module}/.terraform/tmp/configs/systctl/99-k8s.conf"
-  content         = file("${path.module}/templates/configs/systctl/99-k8s.conf")
-  file_permission = "0644"
-}
-
-resource "local_file" "config_tmpfiles_rancher" {
-  filename        = "${path.module}/.terraform/tmp/configs/tmpfiles/var-lib-rancher.conf"
-  content         = file("${path.module}/templates/configs/tmpfiles/var-lib-rancher.conf")
-  file_permission = "0644"
-}
-
-resource "local_file" "script_install_rke2" {
-  filename        = "${path.module}/.terraform/tmp/scripts/install-rke2.sh"
-  content         = file("${path.module}/templates/scripts/install-rke2.sh")
-  file_permission = "0600"
-}
-
-resource "local_file" "script_start_rke2" {
-  filename        = "${path.module}/.terraform/tmp/scripts/start-rke2.sh"
-  content         = file("${path.module}/templates/scripts/start-rke2.sh")
-  file_permission = "0600"
-}
-
-resource "local_file" "manifest_tailscale_operator" {
-  filename = "${path.module}/.terraform/tmp/manifests/tailscale-operator.yaml"
-  content = templatefile("${path.module}/templates/manifests/tailscale-operator.yaml.tftpl", {
-    name          = var.name
-    client_id     = var.tailscale_operator.client_id
-    client_secret = var.tailscale_operator.client_secret
-  })
-  file_permission = "0600"
-}
-
-resource "local_file" "manifest_canal" {
-  filename        = "${path.module}/.terraform/tmp/manifests/rke2-canal-config.yaml"
-  content         = file("${path.module}/templates/manifests/rke2-canal-config.yaml")
-  file_permission = "0600"
-}
-
-resource "local_file" "manifest_coredns" {
-  filename        = "${path.module}/.terraform/tmp/manifests/rke2-coredns-config.yaml"
-  content         = file("${path.module}/templates/manifests/rke2-coredns-config.yaml")
-  file_permission = "0600"
-}
-resource "local_file" "manifest_multus" {
-  filename        = "${path.module}/.terraform/tmp/manifests/rke2-multus-config.yaml"
-  content         = file("${path.module}/templates/manifests/rke2-multus-config.yaml")
-  file_permission = "0600"
+  start_commands = [
+    "systemctl start rke2-server.service"
+  ]
 }
 
 resource "terraform_data" "nishir" {
-  triggers_replace = {
-    config_rke2                 = local_file.config_rke2.id
-    config_sysctl_conf          = local_file.config_sysctl_k8s.id
-    config_tmpfiles_conf        = local_file.config_tmpfiles_rancher.id
-    manifest_canal              = local_file.manifest_canal.id
-    manifest_coredns            = local_file.manifest_coredns.id
-    manifest_multus             = local_file.manifest_multus.id
-    manifest_tailscale_operator = local_file.manifest_tailscale_operator.id
-    script_install_rke2         = local_file.script_install_rke2.id
-    script_start_rke2           = local_file.script_start_rke2.id
-  }
-
   connection {
     type = "ssh"
     user = "root"
@@ -87,45 +22,61 @@ resource "terraform_data" "nishir" {
   }
 
   provisioner "file" {
-    source      = local_file.config_sysctl_k8s.filename
+    content     = file("${path.module}/templates/configs/systctl/99-k8s.conf")
     destination = "/etc/sysctl.d/99-k8s.conf"
   }
 
   provisioner "file" {
-    source      = local_file.config_tmpfiles_rancher.filename
+    content     = file("${path.module}/templates/configs/tmpfiles/var-lib-rancher.conf")
     destination = "/etc/tmpfiles.d/var-lib-rancher.conf"
   }
 
   provisioner "remote-exec" {
-    script = local_file.script_install_rke2.filename
+    inline = local.install_commands
   }
 
   provisioner "file" {
-    source      = local_file.config_rke2.filename
+    content = templatefile("${path.module}/templates/configs/rke2/config.yaml.tftpl", {
+      etcd_s3_access_key_id     = var.etcd_snapshot.access_key_id
+      etcd_s3_bucket            = var.etcd_snapshot.bucket
+      etcd_s3_endpoint          = var.etcd_snapshot.endpoint
+      etcd_s3_region            = var.etcd_snapshot.region
+      etcd_s3_secret_access_key = var.etcd_snapshot.secret_access_key
+      node_ip                   = var.rke2.node_ip
+      node_labels = {
+        "beta.kubernetes.io/instance-type" = "rpi5-large"
+        "node.kubernetes.io/instance-type" = "rpi5-large"
+      }
+      tls_san = var.rke2.tls_san
+    })
     destination = "/etc/rancher/rke2/config.yaml"
   }
 
   provisioner "remote-exec" {
-    script = local_file.script_start_rke2.filename
+    inline = local.start_commands
   }
 
   provisioner "file" {
-    content     = local_file.manifest_canal.content
+    content     = file("${path.module}/templates/manifests/rke2-canal-config.yaml")
     destination = "/var/lib/rancher/rke2/server/manifests/rke2-canal-config.yaml"
   }
 
   provisioner "file" {
-    content     = local_file.manifest_coredns.content
+    content     = file("${path.module}/templates/manifests/rke2-coredns-config.yaml")
     destination = "/var/lib/rancher/rke2/server/manifests/rke2-coredns-config.yaml"
   }
 
   provisioner "file" {
-    content     = local_file.manifest_multus.content
+    content     = file("${path.module}/templates/manifests/rke2-multus-config.yaml")
     destination = "/var/lib/rancher/rke2/server/manifests/rke2-multus-config.yaml"
   }
 
   provisioner "file" {
-    content     = local_file.manifest_tailscale_operator.content
+    content = templatefile("${path.module}/templates/manifests/tailscale-operator.yaml.tftpl", {
+      name          = var.name
+      client_id     = var.tailscale_operator.client_id
+      client_secret = var.tailscale_operator.client_secret
+    })
     destination = "/var/lib/rancher/rke2/server/manifests/tailscale-operator.yaml"
   }
 }
