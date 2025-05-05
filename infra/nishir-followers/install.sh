0d0f22f1c99c1bbd4e5bc18f298a18caba5b42fb
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-05-06 01:06:32 +0200

    Fix followers IPs
    [ghstack-poisoned]

--- infra/nishir-followers/install.sh
+++ infra/nishir-followers/install.sh
@@ -7,6 +7,10 @@ set -o pipefail
 # Find the nishir server name from tailscale
 SERVER=$(tailscale status -json | jq -r '.Peer[] | select(.HostName == "nishir") | .DNSName' | sed 's/\.$//')
 
+# Get node IPs for minish and fushi from tailscale
+MINISH_IP=$(tailscale status -json | jq -r '.Peer[] | select(.HostName == "minish") | .TailscaleIPs[0]')
+FUSHI_IP=$(tailscale status -json | jq -r '.Peer[] | select(.HostName == "fushi") | .TailscaleIPs[0]')
+
 # SSH into nishir and retrieve the server token
 NODE_TOKEN=$(ssh "root@${SERVER}" "cat /var/lib/rancher/rke2/server/node-token")
 
@@ -14,7 +18,18 @@ NODE_TOKEN=$(ssh "root@${SERVER}" "cat /var/lib/rancher/rke2/server/node-token")
 jq -n \
   --arg node_token "${NODE_TOKEN}" \
   --arg server "${SERVER}" \
-  '{rke2: {node_token: $node_token, server: $server}}' >"$(dirname "$0")/terraform.tfvars.json"
+  --arg minish_ip "${MINISH_IP}" \
+  --arg fushi_ip "${FUSHI_IP}" \
+  '{
+    nodes: {
+      minish: { node_ip: $minish_ip },
+      fushi: { node_ip: $fushi_ip }
+    },
+    rke2: {
+      node_token: $node_token,
+      server: $server,
+    }
+  }' >"$(dirname "$0")/terraform.tfvars.json"
 
 # Set secure permissions for the tfvars file
 chmod 600 "$(dirname "$0")/terraform.tfvars.json"
