204a1b2e7bb86f17a8edcf626db9c328d4f28d06
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-06-17 12:27:10 +0900

    Merge Nix configs
    ghstack-source-id: c8092a0528fb9a08bb47ba8e0349ab99886915cd
    Pull Request resolved: https://github.com/shikanime/manifests/pull/440

--- flake.nix
+++ flake.nix
@@ -28,8 +28,96 @@
       imports = [
         devenv.flakeModule
         treefmt-nix.flakeModule
-        ./nix/modules/flake/devenv.nix
       ];
+      perSystem =
+        { self', pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            enableDefaultExcludes = true;
+            programs = {
+              hclfmt.enable = true;
+              nixfmt.enable = true;
+              prettier.enable = true;
+              shfmt.enable = true;
+              statix.enable = true;
+              terraform.enable = true;
+            };
+            settings.global.excludes = [
+              "*.excalidraw"
+              "*.terraform.lock.hcl"
+              ".gitattributes"
+              "LICENSE"
+            ];
+          };
+          devenv.shells.default = {
+            containers = pkgs.lib.mkForce { };
+            languages = {
+              opentofu.enable = true;
+              nix.enable = true;
+            };
+            cachix = {
+              enable = true;
+              push = "shikanime";
+            };
+            git-hooks.hooks = {
+              actionlint.enable = true;
+              deadnix.enable = true;
+              flake-checker.enable = true;
+              shellcheck.enable = true;
+              tflint.enable = true;
+            };
+            packages = [
+              pkgs.clusterctl
+              pkgs.gh
+              pkgs.gitnr
+              pkgs.gnugrep
+              pkgs.gnused
+              pkgs.kubectl
+              pkgs.kubernetes-helm
+              pkgs.kustomize
+              pkgs.skaffold
+              pkgs.skopeo
+              pkgs.yq-go
+              self'.packages.longhornctl
+            ];
+          };
+          packages.longhornctl = pkgs.buildGoModule rec {
+            pname = "longhornctl";
+            version = "1.8.1";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "longhorn";
+              repo = "cli";
+              rev = "v${version}";
+              hash = "sha256-b4baSLYsibdhEgOPbgZxD5M63rdSiztofmB73JPvY4E=";
+            };
+
+            vendorHash = null;
+
+            subPackages = [ "cmd/remote" ];
+
+            ldflags = [
+              "-s"
+              "-w"
+              "-X github.com/longhorn/cli/meta.Version=v${version}"
+              "-X github.com/longhorn/cli/meta.GitCommit=${src.rev}"
+              "-X github.com/longhorn/cli/meta.BuildDate=1970-01-01T00:00:00+00:00"
+            ];
+
+            postInstall = ''
+              mv $out/bin/remote $out/bin/longhornctl
+            '';
+
+            meta = with pkgs.lib; {
+              description = "Longhorn command line tool";
+              homepage = "https://github.com/longhorn/cli";
+              license = licenses.asl20;
+              maintainers = with maintainers; [ shikanime ];
+              mainProgram = "longhornctl";
+            };
+          };
+        };
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
--- update.sh
+++ update.sh
@@ -33,18 +33,32 @@ for app_dir in "$(dirname "$0")"/apps/* "$(dirname "$0")"/clusters/*; do
   done
 done
 
-for dir in "$(dirname "$0")"/nix/packages/*; do
-  if [ -f "$dir/update.sh" ]; then
-    bash "$dir/update.sh" 2>&1 |
-      sed 's/^/['"$(basename "$(dirname "$dir")")"'] /' &
-  fi
-done
-
 for dir in "$(dirname "$0")"/infra/*; do
   if [ -f "$dir/update.sh" ]; then
     bash "$dir/update.sh" 2>&1 |
       sed 's/^/['"$(basename "$dir")"'] /' &
   fi
 done
+
+# Update Longhorn package
+LATEST_VERSION=$(
+  curl --silent \
+    https://api.github.com/repos/longhorn/cli/releases/latest |
+    jq -r '.tag_name | ltrimstr("v")'
+)
+
+COMMIT=$(
+  curl --silent \
+    https://api.github.com/repos/longhorn/cli/tags |
+    jq -r "map(select(.name == \"v${LATEST_VERSION}\")) | .[0] | .commit.sha"
+)
+
+GIT_PREFETCH=$(nix-prefetch-url --unpack https://github.com/longhorn/cli/archive/${COMMIT}.tar.gz)
+GIT_HASH=$(nix hash convert --hash-algo sha256 --to sri ${GIT_PREFETCH})
+
+sed -i \
+  -e "s|version = \".*\"|version = \"${LATEST_VERSION:-}\"|" \
+  -e "s|hash = \".*\"|hash = \"${GIT_HASH}\"|" \
+  "$(dirname "$0")/default.nix"
 
 wait
