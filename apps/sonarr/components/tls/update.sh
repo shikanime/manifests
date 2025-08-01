570deece87419fe48b991c936ac94db33ee6cf5a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-08-01 09:26:42 +0900

    Update base for Update on "Increase Jellyfin volume"
    [ghstack-poisoned]

--- apps/sonarr/components/tls/update.sh
+++ apps/sonarr/components/tls/update.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 # Get current password if it exists
-CURRENT_PASSWORD=$(yq '.secretGenerator[0].literals[0]' "$(dirname "$0")/kustomization.yaml" | sed 's/^password=//')
+CURRENT_PASSWORD=$(yq '.secretGenerator[0].literals[0]' "$(dirname "$0")"/kustomization.yaml | sed 's/^password=//')
 
 # Generate new password only if current password is empty
 if [ -z "$CURRENT_PASSWORD" ]; then
@@ -17,10 +17,10 @@ fi
 # Handle SOPS encryption/decryption
 yq -i \
   ".secretGenerator[0].literals[0] = \"password=$PASSWORD\"" \
-  "$(dirname "$0")/kustomization.yaml"
+  "$(dirname "$0")"/kustomization.yaml
 
 sops \
   --encrypt \
   --encrypted-regex "^(literals)$" \
-  "$(dirname "$0")/kustomization.yaml" > \
+  "$(dirname "$0")"/kustomization.yaml > \
   "$(dirname "$0")/kustomization.enc.yaml"
