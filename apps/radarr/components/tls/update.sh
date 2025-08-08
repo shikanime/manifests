d06a91927d9dc90eb3f19f754fc546eed61ed450
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-08-09 01:58:41 +0900

    Update Servarr to CA update
    ghstack-source-id: 6e6d2bccc09d783bc5ee216267c6a8bc24bc32e6
    Pull Request resolved: https://github.com/shikanime/manifests/pull/483

--- apps/radarr/components/tls/update.sh
+++ apps/radarr/components/tls/update.sh
@@ -4,6 +4,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Define container images to check
+declare -A IMAGES=(
+  ["radarr"]="lscr.io/linuxserver/radarr"
+)
+
+for IMAGE_NAME in "${!IMAGES[@]}"; do
+  FULL_IMAGE="${IMAGES[$IMAGE_NAME]}"
+  LATEST_VERSION=$(
+    skopeo list-tags "docker://${FULL_IMAGE}" |
+      jq -r '.Tags | map(select(test("^[0-9]+\\.[0-9]+\\.[0-9]+$"))) | sort_by(split(".") | map(tonumber)) | last'
+  )
+  if [[ -z $LATEST_VERSION ]]; then
+    echo "Image '$FULL_IMAGE' not found in registry."
+  else
+    (cd "$(dirname "$0")" &&
+      kustomize edit set image "${IMAGE_NAME}=${FULL_IMAGE}:${LATEST_VERSION}")
+  fi
+done
+
 # Get current password if it exists
 CURRENT_PASSWORD=$(yq '.secretGenerator[0].literals[0]' "$(dirname "$0")"/kustomization.yaml | sed 's/^password=//')
 
