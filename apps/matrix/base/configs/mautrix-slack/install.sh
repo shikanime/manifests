0bf46fc2113ce975d24a106763cad8b9f37fcd11
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-08-08 23:04:21 +0900

    Update base for Update on "Add probes for Google Chat Matrix brigde"
    [ghstack-poisoned]

--- apps/matrix/base/configs/mautrix-slack/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-sops --decrypt "$(dirname "$0")"/config.enc.yaml >"$(dirname "$0")"/config.yaml
-sops --decrypt "$(dirname "$0")"/registration.enc.yaml >"$(dirname "$0")"/registration.yaml
