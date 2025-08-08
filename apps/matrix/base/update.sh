223c6d4fd3e2e9bdee1c0f4a0744e826a4df87e4
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-08-08 23:52:39 +0900

    Move configs to cluster specific
    [ghstack-poisoned]

--- apps/matrix/base/update.sh
+++ apps/matrix/base/update.sh
@@ -4,13 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-for dir in "$(dirname "$0")"/configs/*; do
-  if [ -f "$dir/update.sh" ]; then
-    bash "$dir/update.sh" 2>&1 |
-      sed 's/^/['"$(basename "$dir")"'] /' &
-  fi
-done
-
 # Define container images to check
 declare -A IMAGES=(
   ["busybox"]="docker.io/library/busybox"
