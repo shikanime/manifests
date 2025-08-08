7e40fbba0766f06de185407647842d21ce980b6c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-08-09 02:07:03 +0900

    Migrate Matrix
    ghstack-source-id: b04e6a23d4326eab6e666c03cca3e54e3363da44
    Pull Request resolved: https://github.com/shikanime/manifests/pull/488

--- apps/matrix/components/tls/update.sh
+++ apps/matrix/components/tls/update.sh
@@ -4,6 +4,28 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Define container images to check
+declare -A IMAGES=(
+  ["synapse"]="docker.io/matrixdotorg/synapse"
+)
+
+for IMAGE_NAME in "${!IMAGES[@]}"; do
+  FULL_IMAGE="${IMAGES[$IMAGE_NAME]}"
+  LATEST_VERSION=$(
+    skopeo list-tags "docker://${FULL_IMAGE}" |
+      jq -r '.Tags | map(select(test("^v?[0-9]+\\.[0-9]+\\.[0-9]+$"))) | sort_by(. | split("[.-]") | map(tonumber? // 0)) | last'
+  )
+  if [[ -z $LATEST_VERSION ]]; then
+    echo "Image '$FULL_IMAGE' not found in registry."
+  else
+    (cd "$(dirname "$0")" &&
+      kustomize edit set image "${IMAGE_NAME}=${FULL_IMAGE}:${LATEST_VERSION}")
+    yq -i \
+      ".labels.[].pairs.[\"app.kubernetes.io/version\"] = \"${LATEST_VERSION#v}\"" \
+      "$(dirname "$0")"/kustomization.yaml
+  fi
+done
+
 # Get current password if it exists
 CURRENT_PASSWORD=$(yq '.secretGenerator[0].literals[0]' "$(dirname "$0")"/kustomization.yaml | sed 's/^password=//')
 
