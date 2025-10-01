76cbf874003c04229e19699972a80ba79437a9ab
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-02 01:53:59 +0200

    Remove useless Gitea keystore
    ghstack-source-id: dba51c92257f6ac04610e46c4fe05060aa23c494
    Pull Request resolved: https://github.com/shikanime/manifests/pull/525

--- apps/gitea/components/tls/update.sh
+++ apps/gitea/components/tls/update.sh
@@ -23,24 +23,3 @@ for IMAGE_NAME in "${!IMAGES[@]}"; do
       kustomize edit set image "${IMAGE_NAME}=${FULL_IMAGE}:${LATEST_VERSION}")
   fi
 done
-
-# Get current password if it exists
-CURRENT_PASSWORD=$(yq '.secretGenerator[0].literals[0]' "$(dirname "$0")"/kustomization.yaml | sed 's/^password=//')
-
-# Generate new password only if current password is empty
-if [ -z "$CURRENT_PASSWORD" ]; then
-  PASSWORD=$(openssl rand -base64 32)
-else
-  PASSWORD="$CURRENT_PASSWORD"
-fi
-
-# Handle SOPS encryption/decryption
-yq -i \
-  ".secretGenerator[0].literals[0] = \"password=$PASSWORD\"" \
-  "$(dirname "$0")"/kustomization.yaml
-
-sops \
-  --encrypt \
-  --encrypted-regex "^(literals)$" \
-  "$(dirname "$0")"/kustomization.yaml > \
-  "$(dirname "$0")"/kustomization.enc.yaml
