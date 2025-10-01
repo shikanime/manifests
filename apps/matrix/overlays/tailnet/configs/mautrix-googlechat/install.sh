1d5a4ff3c6c048263f587eb9a2027f229d0d7149
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-01 23:37:29 +0200

    Fix Matrix install
    [ghstack-poisoned]

--- apps/matrix/overlays/tailnet/configs/mautrix-googlechat/install.sh
+++ apps/matrix/overlays/tailnet/configs/mautrix-googlechat/install.sh
@@ -8,11 +8,11 @@ set -o pipefail
 sops \
   --encrypt \
   --encrypted-regex "^(as_token|hs_token|shared_secret)$" \
-  "$(dirname "$0")"/config.yaml > \
-  "$(dirname "$0")"/config.enc.yaml
+  "$(dirname "$0")"/config.enc.yaml > \
+  "$(dirname "$0")"/config.yaml
 
 sops \
   --encrypt \
   --encrypted-regex "^(as_token|hs_token)$" \
-  "$(dirname "$0")"/registration.yaml > \
-  "$(dirname "$0")"/registration.enc.yaml
+  "$(dirname "$0")"/registration.enc.yaml > \
+  "$(dirname "$0")"/registration.yaml
--- apps/matrix/overlays/tailnet/configs/mautrix-slack/install.sh
+++ apps/matrix/overlays/tailnet/configs/mautrix-slack/install.sh
@@ -5,5 +5,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-sops --decrypt "$(dirname "$0")"/config.enc.yaml >"$(dirname "$0")"/config.yaml
-sops --decrypt "$(dirname "$0")"/registration.enc.yaml >"$(dirname "$0")"/registration.yaml
+sops \
+  --encrypt \
+  --encrypted-regex "^(as_token|hs_token|shared_secret)$" \
+  "$(dirname "$0")"/config.enc.yaml > \
+  "$(dirname "$0")"/config.yaml
+
+sops \
+  --encrypt \
+  --encrypted-regex "^(as_token|hs_token)$" \
+  "$(dirname "$0")"/registration.enc.yaml > \
+  "$(dirname "$0")"/registration.yaml
