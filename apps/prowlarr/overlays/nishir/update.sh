a1064144358847e3af48b8eb81a26c80ec252b2d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-05 23:46:34 +0200

    Cleanup TLS component
    [ghstack-poisoned]

--- apps/prowlarr/overlays/nishir/update.sh
+++ apps/prowlarr/overlays/nishir/update.sh
@@ -5,25 +5,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Define container images to check
-declare -A IMAGES=(
-  ["whisparr"]="ghcr.io/hotio/whisparr"
-)
-
-for IMAGE_NAME in "${!IMAGES[@]}"; do
-  FULL_IMAGE="${IMAGES[$IMAGE_NAME]}"
-  LATEST_VERSION=$(
-    skopeo list-tags "docker://${FULL_IMAGE}" |
-      jq -r '.Tags | map(select(test("^v[0-9]+[.-].*[0-9]+\\.[0-9]+$"))) | sort_by(. | split("[.-]") | map(tonumber? // 0)) | last'
-  )
-  if [[ -z $LATEST_VERSION ]]; then
-    echo "Image '$FULL_IMAGE' not found in registry."
-  else
-    (cd "$(dirname "$0")" &&
-      kustomize edit set image "${IMAGE_NAME}=${FULL_IMAGE}:${LATEST_VERSION}")
-  fi
-done
-
 # Get current password if it exists
 CURRENT_PASSWORD=$(yq '.secretGenerator[0].literals[0]' "$(dirname "$0")"/kustomization.yaml | sed 's/^password=//')
 
