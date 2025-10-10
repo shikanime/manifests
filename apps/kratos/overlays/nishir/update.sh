2159f87f83e2dcd8ce0272e89d4e66a0278f692c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-10 13:39:38 +0200

    Add Kratos
    [ghstack-poisoned]

---
+++ apps/kratos/overlays/nishir/update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env nix
+#! nix develop --impure --command bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+sops \
+  --encrypt \
+  "$(dirname "$0")"/kratos/.env > \
+  "$(dirname "$0")"/kratos/.enc.env
