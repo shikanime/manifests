4c0dcc1bfbbc83440e905e2062e931d0692265e1
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-10-11 13:31:18 +0200

    Add Kratos
    ghstack-source-id: 41518a161163ed5404008e671181bd4b883b1227
    Pull Request resolved: https://github.com/shikanime/manifests/pull/579

---
+++ apps/kratos/overlays/nishir/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env nix
+#! nix develop --impure --command bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+sops \
+  --decrypt \
+  "$(dirname "$0")"/kratos/.enc.env > \
+  "$(dirname "$0")"/kratos/.env
