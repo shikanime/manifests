1a7e6bf9a1bbd2e275d65a9b1d539500a18755f2
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-11 22:53:56 +0100

    Migrate to devlib and automata Flake module
    [ghstack-poisoned]

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
+    automata.url = "github:shikanime-studio/automata";
     devenv.url = "github:cachix/devenv";
     devlib.url = "github:shikanime-studio/devlib";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks.url = "github:cachix/git-hooks.nix";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
@@ -22,78 +24,72 @@
 
   outputs =
     inputs@{
+      automata,
       devenv,
       devlib,
       flake-parts,
+      git-hooks,
       treefmt-nix,
       ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
+        devlib.flakeModule
         devenv.flakeModule
+        git-hooks.flakeModule
         treefmt-nix.flakeModule
       ];
       perSystem =
-        { self', pkgs, ... }:
         {
-          treefmt = {
-            projectRootFile = "flake.nix";
-            enableDefaultExcludes = true;
-            programs = {
-              gofmt.enable = true;
-              hclfmt.enable = true;
-              nixfmt.enable = true;
-              prettier.enable = true;
-              shfmt.enable = true;
-              statix.enable = true;
-              terraform.enable = true;
+          self',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devenv.shells.default = {
+            cachix = {
+              enable = true;
+              push = "shikanime";
             };
-            settings.global.excludes = [
-              "*.excalidraw"
-              "*.terraform.lock.hcl"
-              ".gitattributes"
-              "LICENSE"
+            containers = pkgs.lib.mkForce { };
+            gitignore = {
+              enable = true;
+              enableDefaultTemplates = true;
+              content = [
+                "config.xml"
+              ];
+            };
+            github.enable = true;
+            languages = {
+              nix.enable = true;
+              opentofu.enable = true;
+            };
+            packages = [
+              pkgs.clusterctl
+              pkgs.gh
+              pkgs.gnugrep
+              pkgs.gnused
+              pkgs.k0sctl
+              pkgs.kubectl
+              pkgs.kubernetes-helm
+              pkgs.kustomize
+              pkgs.sapling
+              pkgs.skaffold
+              pkgs.skopeo
+              pkgs.sops
+              pkgs.yq-go
+              self'.packages.longhornctl
+              automata.packages.${system}.default
             ];
-          };
-          devenv = {
-            modules = [
-              devlib.devenvModule
-            ];
-            shells.default = {
-              cachix = {
-                enable = true;
-                push = "shikanime";
-              };
-              containers = pkgs.lib.mkForce { };
-              gitignore = {
-                enable = true;
-                enableDefaultTemplates = true;
-                content = [
-                  "config.xml"
+            treefmt = {
+              config = {
+                enableDefaultExcludes = true;
+                programs.prettier.enable = true;
+                settings.global.excludes = [
+                  "*.excalidraw"
                 ];
               };
-              github.enable = true;
-              languages = {
-                go.enable = true;
-                nix.enable = true;
-                opentofu.enable = true;
-              };
-              packages = [
-                pkgs.clusterctl
-                pkgs.gh
-                pkgs.gnugrep
-                pkgs.gnused
-                pkgs.k0sctl
-                pkgs.kubectl
-                pkgs.kubernetes-helm
-                pkgs.kustomize
-                pkgs.sapling
-                pkgs.skaffold
-                pkgs.skopeo
-                pkgs.sops
-                pkgs.yq-go
-                self'.packages.longhornctl
-              ];
             };
           };
           packages.longhornctl = pkgs.buildGoModule rec {
