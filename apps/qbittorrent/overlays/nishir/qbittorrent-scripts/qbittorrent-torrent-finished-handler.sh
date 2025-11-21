12139d593843f150dfb92a5aa67c0d39b433fc2a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-21 22:55:17 +0100

    None on "Add qBittorrent handler"
    [ghstack-poisoned]

--- apps/qbittorrent/overlays/nishir/qbittorrent-scripts/qbittorrent-torrent-finished-handler.sh
+++ apps/qbittorrent/overlays/nishir/qbittorrent-scripts/qbittorrent-torrent-finished-handler.sh
@@ -4,19 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-CATEGORY="$1"
-CONTENT_PATH="$2"
-
-if [ -z "$CATEGORY" ]; then
+if [ -z "$1" ]; then
     echo "ERROR: Category is empty. Configure qBittorrent to pass the category (%L) as the first argument." >&2
     exit 1
 fi
 
-if [ -z "$CONTENT_PATH" ]; then
+if [ -z "$2" ]; then
     echo "ERROR: Content Path is empty. Configure qBittorrent to pass the content path (%F) as the second argument." >&2
     exit 1
 fi
 
-if [ "$CATEGORY" == "prowlarr" ]; then
-    cp -r "$CONTENT_PATH" "/data/staging/Downloads"
+if [ "$1" == "prowlarr" ]; then
+    cp -r "$2" "/data/staging/Downloads"
 fi
