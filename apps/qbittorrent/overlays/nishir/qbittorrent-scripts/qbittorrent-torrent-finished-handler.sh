c563a1b5eb6e047a62a20b4fb78bba4a62250609
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-11-21 23:53:46 +0100

    Fix unset args

--- apps/qbittorrent/overlays/nishir/qbittorrent-scripts/qbittorrent-torrent-finished-handler.sh
+++ apps/qbittorrent/overlays/nishir/qbittorrent-scripts/qbittorrent-torrent-finished-handler.sh
@@ -4,16 +4,19 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [ -z "$1" ]; then
+CATEGORY="${1:-}"
+CONTENT_PATH="${2:-}"
+
+if [ -z "$CATEGORY" ]; then
     echo "ERROR: Category is empty. Configure qBittorrent to pass the category (%L) as the first argument." >&2
     exit 1
 fi
 
-if [ -z "$2" ]; then
+if [ -z "$CONTENT_PATH" ]; then
     echo "ERROR: Content Path is empty. Configure qBittorrent to pass the content path (%F) as the second argument." >&2
     exit 1
 fi
 
-if [ "$1" == "prowlarr" ]; then
-    cp -r "$2" "/data/staging/Downloads"
+if [ "$CATEGORY" == "prowlarr" ]; then
+    cp -r "$CONTENT_PATH" "/data/staging/Downloads"
 fi
