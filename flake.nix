1f97dd26cb50aa35f4438ccec574179af1688a8d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-04-01 13:36:41 +0200

    Update
    [ghstack-poisoned]

--- flake.nix
+++ flake.nix
@@ -154,10 +154,10 @@
                 };
 
                 gitignore.content = [
-                  "/apps/matrix/overlays/*/synapse/homeserver.yaml"
-                  "/apps/matrix/overlays/*/mautrix-*/config.yaml"
-                  "/apps/matrix/overlays/*/mautrix-*/doublepuppet.yaml"
-                  "/apps/matrix/overlays/*/mautrix-*/registration.yaml"
+                  "apps/matrix/overlays/*/synapse/homeserver.yaml"
+                  "apps/matrix/overlays/*/mautrix-*/config.yaml"
+                  "apps/matrix/overlays/*/mautrix-*/doublepuppet.yaml"
+                  "apps/matrix/overlays/*/mautrix-*/registration.yaml"
                 ];
 
                 packages = [
