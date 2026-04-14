d039c25edc7e79b858d6a1de6214cd0662cafe16
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-04-14 23:49:27 +0200

    Split Matrix in components
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: Ic747c51ce495346b2188ec673d86e6436a6a6964
    ghstack-source-id: a7b10876cb2182015b1acdf07c7d7caa38cec895
    Pull-Request: https://github.com/shikanime/manifests/pull/1066

--- flake.nix
+++ flake.nix
@@ -107,17 +107,17 @@
                           key_groups = keyGroups;
                         }
                         {
-                          path_regex = "apps/matrix/overlays/.*/synapse/homeserver\\..*";
+                          path_regex = "apps/synapse/overlays/.*/synapse/homeserver\\..*";
                           encrypted_regex = "^(registration_shared_secret|form_secret|macaroon_secret_key)$";
                           key_groups = keyGroups;
                         }
                         {
-                          path_regex = "apps/matrix/overlays/.*/mautrix-.*/config\\..*";
+                          path_regex = "apps/mautrix/.*/overlays/.*/matrix-.*/config\\..*";
                           encrypted_regex = "^(avatar_proxy_key|as_token|hs_token|pickle_key|server_key|shared_secret|signing_key|login_shared_secret_map|secrets)$";
                           key_groups = keyGroups;
                         }
                         {
-                          path_regex = "apps/matrix/overlays/.*/mautrix-.*/(registration|doublepuppet)*\\..*";
+                          path_regex = "apps/mautrix/.*/overlays/.*/matrix-.*/(registration|doublepuppet)\\..*";
                           encrypted_regex = "^(as_token|hs_token)$";
                           key_groups = keyGroups;
                         }
