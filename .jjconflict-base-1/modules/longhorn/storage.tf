c487d9ca57a845101da33e2a8125d12d16aa045d
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-04-14 19:48:57 +0200

    Split Matrix in components
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: Ic747c51ce495346b2188ec673d86e6436a6a6964
    ghstack-source-id: 323d25421f3798259f8d3e8b0eafd62b83140b2a
    Pull-Request: https://github.com/shikanime/manifests/pull/1065

--- .jjconflict-base-1/modules/longhorn/storage.tf
+++
@@ -1,8 +0,0 @@
-resource "random_id" "longhorn_backups" {
-  byte_length = 4
-  prefix      = "${var.project}-${var.name}-longhorn-backups-"
-}
-
-resource "aws_s3_bucket" "longhorn_backups" {
-  bucket = random_id.longhorn_backups.hex
-}
